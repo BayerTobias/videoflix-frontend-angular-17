960f1908a8f6a440437785523ae09222f12dd092
BayerTobias <tbayer2@gmx.de>
2024-06-02 20:28:57 +0200

    legal components styling

--- src/app/legal/components/legal-notice/legal-notice.component.ts
+++ src/app/legal/components/legal-notice/legal-notice.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrl: './legal-notice.component.scss',
 })
-export class LegalNoticeComponent {
-
-}
+export class LegalNoticeComponent {}
--- src/app/legal/components/privacy-policy/privacy-policy.component.ts
+++ src/app/legal/components/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
-export class PrivacyPolicyComponent {
-
-}
+export class PrivacyPolicyComponent {}
